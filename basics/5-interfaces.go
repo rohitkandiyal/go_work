fbaaf14d72f58b344197b13532adecc55f2dad0c
Rohit Kandiyal <Kandiyal.Rohit@bcg.com>
2023-12-30 10:38:34 +0530

    added interface

--- basics/5-interfaces.go
+++ basics/5-interfaces.go
@@ -1,9 +1,37 @@
-package main
+// Interface is a collection of abstract method signatures. The data type which implements this interface must define these methods as concrete.
 
+package main
 import (
 	"fmt"
 )
 
+// Defining interface
+
+type shape interface {
+	area() float64
+	perimeter() float64
+}
+
+
+// Now rectange below implements this interface
+
+type rectange struct {
+	length, breadth float64
+}
+func (r rectange) area() float64 {				// Struct method
+	return r.length * r.breadth
+}
+func (r rectange) perimeter() float64 {			// Struct method
+	return 2 * (r.length + r.breadth)
+}
+
+// Now circle below too can similarily implement shape interface
+
 func main()  {
-	
+	my_rect := rectange {
+		length: 6.50,
+		breadth: 8.50,
+	}
+	fmt.Println(my_rect.area())
+	fmt.Println(my_rect.perimeter())
 }
