92f088a6419cf8fd92318cd0cad71975d69c94c9
Rohit Kandiyal <rkandiyal@zscaler.com>
2024-05-21 22:22:41 +0530

    adding external third party module

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 
 	"github.com/rohitkandiyal/go_work/pkg/test_package"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
 
-	fmt.Println("main.go: Hello from go_work module main...!")
-	test_package.PrintHello()
+	cmd := &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("main.go: Hello from go_work module main...!")
+			test_package.PrintHello()
+		},
+	}
+
+	fmt.Println("Calling cmd.Execute()!")
+	cmd.Execute()
 
 }
